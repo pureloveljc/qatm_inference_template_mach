ab45df9037efcad99b5f6a9d70dd136e5442896e
purelove <32063794+pureloveljc@users.noreply.github.com>
2023-02-17 10:59:46 +0800

    Create qatm_inference.py
    template match inference

---
+++ qatm_inference.py
@@ -0,0 +1,396 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.3'
+#       jupytext_version: 1.0.5
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from pathlib import Path
+from seaborn import color_palette
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from torchvision import models, transforms, utils
+import copy
+from utils import *
+# %matplotlib inline
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+# # CONVERT IMAGE TO TENSOR
+from clip_sim import Clipinference
+
+transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406],
+                    std=[0.229, 0.224, 0.225],
+                )
+            ])
+
+class ImageDataset(torch.utils.data.Dataset):
+    def __init__(self, template_dir_path, image_name, thresh_csv=None, transform=None):
+        self.transform = transform
+        if not self.transform:
+            self.transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406],
+                    std=[0.229, 0.224, 0.225],
+                )
+            ])
+        self.template_path = template_dir_path
+        self.image_name = image_name
+
+        self.image_raw = cv2.imread(image_name)
+
+        self.thresh_df = None
+        if thresh_csv:
+            self.thresh_df = pd.read_csv(thresh_csv)
+
+        if self.transform:
+            self.image = self.transform(self.image_raw).unsqueeze(0)
+
+    def __len__(self):
+        return len(self.template_names)
+
+    def __getitem__(self, idx):
+        template_path = str(self.template_path)
+        template = cv2.imread(template_path)
+        if self.transform:
+            template = self.transform(template)
+        thresh = 0.7
+        if self.thresh_df is not None:
+            if self.thresh_df.path.isin([template_path]).sum() > 0:
+                thresh = float(self.thresh_df[self.thresh_df.path == template_path].thresh)
+        return {'image': self.image,
+                'image_raw': self.image_raw,
+                'image_name': self.image_name,
+                'template': template.unsqueeze(0),
+                'template_name': template_path,
+                'template_h': template.size()[-2],
+                'template_w': template.size()[-1],
+                'thresh': thresh}
+
+
+template_path = '/home/all/ljc/1226_machine/small_jpg/web/1400X1050_1440X900/4/_small_jpg/8.jpg'
+image_path = '/home/all/ljc/1226_machine/small_jpg/web/1400X1050_1440X900/4/2.png'
+template = cv2.imread(template_path)
+template = transform(template)
+template = template.unsqueeze(0)
+image = transform(cv2.imread(image_path)).unsqueeze(0)
+# dataset = ImageDataset(template_dir, image_path, thresh_csv='thresh_template.csv')
+# model, template, image, image_name
+
+# ### EXTRACT FEATURE
+
+class Featex():
+    def __init__(self, model, use_cuda):
+        self.use_cuda = use_cuda
+        self.feature1 = None
+        self.feature2 = None
+        self.model = copy.deepcopy(model.eval())
+        self.model = self.model[:17]
+        for param in self.model.parameters():
+            param.requires_grad = False
+        if self.use_cuda:
+            self.model = self.model.cuda()
+        self.model[2].register_forward_hook(self.save_feature1)
+        self.model[16].register_forward_hook(self.save_feature2)
+
+    def save_feature1(self, module, input, output):
+        self.feature1 = output.detach()
+
+    def save_feature2(self, module, input, output):
+        self.feature2 = output.detach()
+
+    def __call__(self, input, mode='big'):
+        if self.use_cuda:
+            input = input.cuda()
+        _ = self.model(input)
+        if mode == 'big':
+            # resize feature1 to the same size of feature2
+            self.feature1 = F.interpolate(self.feature1, size=(self.feature2.size()[2], self.feature2.size()[3]),
+                                          mode='bilinear', align_corners=True)
+        else:
+            # resize feature2 to the same size of feature1
+            self.feature2 = F.interpolate(self.feature2, size=(self.feature1.size()[2], self.feature1.size()[3]),
+                                          mode='bilinear', align_corners=True)
+        return torch.cat((self.feature1, self.feature2), dim=1)
+
+
+class MyNormLayer():
+    def __call__(self, x1, x2):
+        bs, _, H, W = x1.size()
+        _, _, h, w = x2.size()
+        x1 = x1.view(bs, -1, H * W)
+        x2 = x2.view(bs, -1, h * w)
+        concat = torch.cat((x1, x2), dim=2)
+        x_mean = torch.mean(concat, dim=2, keepdim=True)
+        x_std = torch.std(concat, dim=2, keepdim=True)
+        x1 = (x1 - x_mean) / x_std
+        x2 = (x2 - x_mean) / x_std
+        x1 = x1.view(bs, -1, H, W)
+        x2 = x2.view(bs, -1, h, w)
+        return [x1, x2]
+
+
+class CreateModel():
+    def __init__(self, alpha, model, use_cuda):
+        self.alpha = alpha
+        self.featex = Featex(model, use_cuda)
+        self.I_feat = None
+        self.I_feat_name = None
+
+    def __call__(self, template, image, image_name):
+        T_feat = self.featex(template)
+        if self.I_feat_name is not image_name:
+            self.I_feat = self.featex(image)
+            self.I_feat_name = image_name
+        conf_maps = None
+        batchsize_T = T_feat.size()[0]
+        for i in range(batchsize_T):
+            T_feat_i = T_feat[i].unsqueeze(0)
+            I_feat_norm, T_feat_i = MyNormLayer()(self.I_feat, T_feat_i)
+            dist = torch.einsum("xcab,xcde->xabde", I_feat_norm / torch.norm(I_feat_norm, dim=1, keepdim=True),
+                                T_feat_i / torch.norm(T_feat_i, dim=1, keepdim=True))
+            conf_map = QATM(self.alpha)(dist)
+            if conf_maps is None:
+                conf_maps = conf_map
+            else:
+                conf_maps = torch.cat([conf_maps, conf_map], dim=0)
+        return conf_maps
+
+
+class QATM():
+    def __init__(self, alpha):
+        self.alpha = alpha
+
+    def __call__(self, x):
+        batch_size, ref_row, ref_col, qry_row, qry_col = x.size()
+        x = x.view(batch_size, ref_row * ref_col, qry_row * qry_col)
+        xm_ref = x - torch.max(x, dim=1, keepdim=True)[0]
+        xm_qry = x - torch.max(x, dim=2, keepdim=True)[0]
+        confidence = torch.sqrt(F.softmax(self.alpha * xm_ref, dim=1) * F.softmax(self.alpha * xm_qry, dim=2))
+        conf_values, ind3 = torch.topk(confidence.cpu(), 1)
+        ind1, ind2 = torch.meshgrid(torch.arange(batch_size), torch.arange(ref_row * ref_col))
+        ind1 = ind1.flatten()
+        ind2 = ind2.flatten()
+        ind3 = ind3.flatten()
+        if x.is_cuda:
+            ind1 = ind1.cuda()
+            ind2 = ind2.cuda()
+
+        values = confidence[ind1, ind2, ind3]
+        values = torch.reshape(values, [batch_size, ref_row, ref_col, 1])
+        return values
+
+    def compute_output_shape(self, input_shape):
+        bs, H, W, _, _ = input_shape
+        return (bs, H, W, 1)
+
+
+# # NMS AND PLOT
+
+# ## SINGLE
+
+def nms(score, w_ini, h_ini, thresh=0.7):
+    dots = np.array(np.where(score > thresh * score.max()))
+
+    x1 = dots[1] - w_ini // 2
+    x2 = x1 + w_ini
+    y1 = dots[0] - h_ini // 2
+    y2 = y1 + h_ini
+
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+    scores = score[dots[0], dots[1]]
+    order = scores.argsort()[::-1]
+
+    keep = []
+    while order.size > 0:
+        i = order[0]
+        keep.append(i)
+        xx1 = np.maximum(x1[i], x1[order[1:]])
+        yy1 = np.maximum(y1[i], y1[order[1:]])
+        xx2 = np.minimum(x2[i], x2[order[1:]])
+        yy2 = np.minimum(y2[i], y2[order[1:]])
+
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+        inter = w * h
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+
+        inds = np.where(ovr <= 0.5)[0]
+        order = order[inds + 1]
+    boxes = np.array([[x1[keep], y1[keep]], [x2[keep], y2[keep]]]).transpose(2, 0, 1)
+    return boxes
+
+
+def plot_result(image_raw, boxes, show=False, save_name=None, color=(255, 0, 0)):
+    # plot result
+    d_img = image_raw.copy()
+    for box in boxes:
+        d_img = cv2.rectangle(d_img, tuple(box[0]), tuple(box[1]), color, 3)
+    if show:
+        plt.imshow(d_img)
+    if save_name:
+        cv2.imwrite(save_name, d_img[:, :, ::-1])
+    return d_img
+
+
+# ## MULTI
+
+def nms_multi(scores, w_array, h_array, thresh_list):
+    indices = np.arange(scores.shape[0])
+    maxes = np.max(scores.reshape(scores.shape[0], -1), axis=1)
+    # omit not-matching templates
+    scores_omit = scores[maxes > 0.1 * maxes.max()]
+    indices_omit = indices[maxes > 0.1 * maxes.max()]
+    # extract candidate pixels from scores
+    dots = None
+    dos_indices = None
+    for index, score in zip(indices_omit, scores_omit):
+        dot = np.array(np.where(score > thresh_list[index] * score.max()))
+        if dots is None:
+            dots = dot
+            dots_indices = np.ones(dot.shape[-1]) * index
+        else:
+            dots = np.concatenate([dots, dot], axis=1)
+            dots_indices = np.concatenate([dots_indices, np.ones(dot.shape[-1]) * index], axis=0)
+    dots_indices = dots_indices.astype(np.int)
+    x1 = dots[1] - w_array[dots_indices] // 2
+    x2 = x1 + w_array[dots_indices]
+    y1 = dots[0] - h_array[dots_indices] // 2
+    y2 = y1 + h_array[dots_indices]
+
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+    scores = scores[dots_indices, dots[0], dots[1]]
+    order = scores.argsort()[::-1]
+    dots_indices = dots_indices[order]
+
+    keep = []
+    keep_index = []
+    while order.size > 0:
+        i = order[0]
+        index = dots_indices[0]
+        keep.append(i)
+        keep_index.append(index)
+        xx1 = np.maximum(x1[i], x1[order[1:]])
+        yy1 = np.maximum(y1[i], y1[order[1:]])
+        xx2 = np.minimum(x2[i], x2[order[1:]])
+        yy2 = np.minimum(y2[i], y2[order[1:]])
+
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+        inter = w * h
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+
+        inds = np.where(ovr <= 0.05)[0]
+        order = order[inds + 1]
+        dots_indices = dots_indices[inds + 1]
+
+    boxes = np.array([[x1[keep], y1[keep]], [x2[keep], y2[keep]]]).transpose(2, 0, 1)
+    return boxes, np.array(keep_index)
+
+
+def plot_result_multi(image_raw, boxes, indices, show=False, save_name=None, color_list=None):
+    d_img = image_raw.copy()
+    if color_list is None:
+        color_list = color_palette("hls", indices.max() + 1)
+        color_list = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), color_list))
+    for i in range(len(indices)):
+        d_img = plot_result(d_img, boxes[i][None, :, :].copy(), color=color_list[indices[i]])
+    if show:
+        plt.imshow(d_img)
+    if save_name:
+        cv2.imwrite(save_name, d_img[:, :, ::-1])
+        print('write success')
+    return d_img
+
+
+# # RUNNING
+
+def run_one_sample(model, template, image, image_name):
+    val = model(template, image, image_name)
+    if val.is_cuda:
+        val = val.cpu()
+    val = val.numpy()
+    val = np.log(val)
+
+    batch_size = val.shape[0]
+    scores = []
+    # print(batch_size)
+    for i in range(batch_size):
+        # compute geometry average on score map
+        # print(i)
+        gray = val[i, :, :, 0]
+        gray = cv2.resize(gray, (image.size()[-1], image.size()[-2]))
+        h = template.size()[-2]
+        w = template.size()[-1]
+        score = compute_score(gray, w, h)
+        score[score > -1e-7] = score.min()
+        score = np.exp(score / (h * w))  # reverse number range back after computing geometry average
+        scores.append(score)
+    return np.array(scores)
+
+
+def run_multi_sample(model, dataset):
+    scores = []
+    w_array = []
+    h_array = []
+    thresh_list = []
+    print((dataset))
+    for idx, data in enumerate(dataset):
+        print(idx)
+        score = run_one_sample(model, data['template'], data['image'], data['image_name'])
+        scores.append(score)
+
+        w_array.append(data['template_w'])
+        h_array.append(data['template_h'])
+        thresh_list.append(data['thresh'])
+    return np.squeeze(np.array(scores), axis=1), np.array(w_array), np.array(h_array), thresh_list
+
+
+model = CreateModel(model=models.vgg19(pretrained=True).features, alpha=25, use_cuda=True)
+print('1')
+import time
+
+while True:
+    start = time.time()
+    score = run_one_sample(model, template, image, image_path)
+    h_array = template.size()[-2]
+    w_array = template.size()[-1]
+    thresh_list = 0.7
+    scores = []
+    w_arrays = []
+    h_arrays = []
+    thresh_lists = []
+    scores.append(score)
+    w_arrays.append(w_array)
+    h_arrays.append(h_array)
+    thresh_lists.append(thresh_list)
+    # np.squeeze(np.array(scores), axis=1), np.array(w_array), np.array(h_array), thresh_list
+
+    print('2')
+    boxes, indices = nms_multi(np.squeeze(np.array(scores), axis=1), np.array(w_arrays), np.array(h_arrays), thresh_lists)
+    print('3')
+    end_t = time.time()
+
+    print(end_t-start)
+    d_img = plot_result_multi(cv2.imread(image_path), boxes, indices, show=False, save_name='result_sample.png')
+# print('4')
+# plt.imshow(scores[2])
+
